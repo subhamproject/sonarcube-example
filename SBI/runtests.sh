a34bf02a2dc9e4326eb849886883cf76825df136
Subham Mandal <subhammandal01@gmail.com>
2019-06-02 00:48:12 +0530

    Update runtests.sh

--- SBI/runtests.sh
+++ SBI/runtests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy
-mvn -s settings.xml clean deploy sonar:sonar -Dsonar.host.url=http://13.229.133.70:9000 -Dsonar.login=$SONAR_TOKEN -Dsonar.projectName=sonar-report
+mvn -U -s settings.xml -Dmaven.package.skip=true -Dmaven.test.skip=true -Dmaven.compile.skip=true deploy sonar:sonar -Dsonar.host.url=http://13.229.133.70:9000 -Dsonar.login=$SONAR_TOKEN -Dsonar.projectName=sonar-report
